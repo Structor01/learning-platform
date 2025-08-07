7efb15f6bce61e4986dc8fb2cfa4ceba90ead7ed
structor01 <arthur.barros@foxgraos.com.br>
2025-08-07 19:50:45 -0300

    SYNC

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,8 +1,8 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 class AdminService {
   constructor() {
-    this.baseURL = `${API_BASE_URL}/admin`;
+    this.baseURL = `${API_BASE_URL}/api/admin`;
   }
 
   async makeRequest(endpoint, options = {}) {
