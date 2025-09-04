829ef1b0723a0691bb516f4cec89b7323c5f3864
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-09-04 09:36:46 -0300

    acrescenta user admin

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -123,7 +123,7 @@ const Navbar = ({ currentView, onViewChange, onAddTrilha, onSearch }) => {
               </a>
 
               {/* Menu Administrador */}
-              {(userData.email === "kauanytorres19@gmail.com" || userData.email === "artfbgyn@gmail.com") && (
+              {(userData.email === "matheuslucasdesouza22@gmail.com" || userData.email === "artfbgyn@gmail.com") && (
                 <DropdownMenu>
                   <DropdownMenuTrigger asChild>
                     <Button
@@ -189,11 +189,11 @@ const Navbar = ({ currentView, onViewChange, onAddTrilha, onSearch }) => {
                       src="/placeholder-avatar.jpg"
                       alt={userData.name}
                     />
-                      <AvatarFallback className="bg-gray-600 text-white">
-                          {userData?.name?.split(" ")
-                              .map((n) => n[0])
-                              .join("")}
-                      </AvatarFallback>
+                    <AvatarFallback className="bg-gray-600 text-white">
+                      {userData?.name?.split(" ")
+                        .map((n) => n[0])
+                        .join("")}
+                    </AvatarFallback>
                   </Avatar>
                 </Button>
               </DropdownMenuTrigger>
