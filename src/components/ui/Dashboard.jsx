f1afce4679de000e37a4482680b8155f86068e81
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-09-08 11:25:15 -0300

    Remove svg do dashboard

--- src/components/ui/Dashboard.jsx
+++ src/components/ui/Dashboard.jsx
@@ -751,10 +751,6 @@ const Dashboard = ({ onCourseSelect = [] }) => {
                         <span className="font-medium">45 min</span>
                       </div>
                       <div className="flex items-center space-x-2">
-                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                        </svg>
-                        <span className="font-medium">Samantha Andrade</span>
                       </div>
                       <div className="flex items-center space-x-2">
                         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
