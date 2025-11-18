04f40b6f065d4927696874877b739caf64d7796c
MatheusLucas01 <matheuslucasdesouza22@gmail.com>
2025-11-18 11:46:57 -0300

    sync

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,4 +1,33 @@
-import { api } from '@/lib/api';
+import { getApiUrl } from '@/components/utils/api';
+
+export function api(path, init) {
+    const accessToken = localStorage.getItem('accessToken')
+    const baseUrl = getApiUrl()
+    const url = path.startsWith('http') ? path : new URL(path, baseUrl).toString()
+
+    const config = {
+        ...init,
+        headers: {
+            ...init?.headers,
+            'Authorization': accessToken ? `Bearer ${accessToken}` : '',
+            // NÃO define Content-Type se for FormData
+            ...(!(init?.body instanceof FormData) && { 'Content-Type': 'application/json' }),
+        }
+    }
+
+    return fetch(url, config)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro na API: ${response.status} - ${response.statusText}`);
+            }
+            const contentType = response.headers.get('content-type');
+            return contentType?.includes('application/json') ? response.json() : response.text();
+        })
+        .catch(error => {
+            console.error('❌ Erro na requisição:', error);
+            throw error;
+        });
+}
 
 export const uploadImage = async (file, folder = 'profile_image') => {
     const formData = new FormData();
